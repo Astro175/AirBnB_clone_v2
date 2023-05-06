b58e3390f651b99a929ceeb4f0706f03dcd73e5c
Astro175 <aspark400@gmail.com>
2023-05-06 22:24:11 +0100

    changed permissions for 0-setup

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -9,7 +9,7 @@ fi
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-sudo touch /data/web_static/releases/test/index.html && echo "<html><head></head><body>Holberton School test</body></html>" | sudo tee /data/web_static/releases/test/index.html
+sudo touch /data/web_static/releases/test/index.html && echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html
 
 sudo rm -f /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
