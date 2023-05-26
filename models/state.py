ec91d86ee196c66fa1f76c05a8c0340e50f3ca21
astro175 <aspark400@gmail.com>
2023-05-26 20:17:20 +0100

    done task 7

--- models/state.py
+++ models/state.py
@@ -13,7 +13,8 @@ class State(BaseModel, Base):
 
     if storage_type == 'db':
         name = Column(String(128), nullable=False)
-        cities = relationship('City', backref='state', cascade='all, delete, delete-orphan')
+        cities = relationship(
+                'City', backref='state', cascade='all, delete, delete-orphan')
     else:
         name = ''
 
